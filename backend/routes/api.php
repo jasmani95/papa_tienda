a95615d88cd4ee0ecbb585bf08d66cc68f69a40a
Your Name <jasmani.chuquimia@uab.edu.bo>
2025-08-03 18:19:49 -0400

    implementacion de registro de usuario

---
+++ backend/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\AuthController;
+
+Route::apiResource('categorias', CategoriaController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class,'login']);
